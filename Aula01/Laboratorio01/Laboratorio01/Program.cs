25ffd6b7f95429c8e189f4c0d016ea4e9a608ccf
Souza <fernanda.souza1@dellteam.com>
2023-08-07 18:29:00 -0300

    Aula02

---
+++ Aula01/Laboratorio01/Laboratorio01/Program.cs
@@ -0,0 +1,23 @@
+﻿byte b;
+b = byte.MaxValue;
+Console.WriteLine("Valor maximo de byte: " + b);
+
+int i;
+i = int.MaxValue;
+Console.WriteLine("Valor maximo de int: " + i);
+long l;
+l = long.MaxValue;
+Console.WriteLine("Valor maximo de long: " + l);
+string strPrimeira = "Alo ";
+string strSegunda = "Mundo";
+string strTerceira = strPrimeira + strSegunda;
+Console.WriteLine(strTerceira);
+int cchTamanho = strTerceira.Length;
+string strQuarta = "Tamanho = " + cchTamanho.ToString();
+Console.WriteLine(strQuarta);
+Console.WriteLine(strTerceira.Substring(0, 5));
+DateTime dt = new DateTime(2015, 04, 23);
+string strQuinta = dt.ToString();
+Console.WriteLine(strQuinta);
+
+
