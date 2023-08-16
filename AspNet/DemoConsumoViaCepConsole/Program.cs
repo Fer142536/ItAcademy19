139e0599530c46cf71508f8192d4adb49b2cc1e8
Souza <fernanda.souza1@dellteam.com>
2023-08-16 17:24:01 -0300

    Aulas C# & Asp.Net

---
+++ AspNet/DemoConsumoViaCepConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿﻿try
+{
+    Console.WriteLine("ConsultarV1:");
+    var resultado = await ViaCepConsumidor.ConsultarV1("93020660");
+    Console.WriteLine(resultado.IsSuccessStatusCode);
+    Console.WriteLine(resultado.StatusCode);
+    var dados = await resultado.Content.ReadAsStringAsync();
+    Console.WriteLine(dados);
+
+    Console.WriteLine("ConsultarV2:");
+    var resultadoV2 = await ViaCepConsumidor.ConsultarV2("93020660");
+    Console.WriteLine(resultadoV2);
+
+    Console.WriteLine("ConsultarV3:");
+    var resultadoV3 = await ViaCepConsumidor.ConsultarV3("93020660");
+    Console.WriteLine(resultadoV3);
+    if (resultadoV3 !=null){
+        Console.WriteLine(resultadoV3.Cep); 
+        Console.WriteLine(resultadoV3.Localidade); 
+        Console.WriteLine(resultadoV3.Uf); 
+        
+    }
+
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("Falha:");
+    Console.WriteLine(e.Message);
+}
---
+++ Aula C#/LaboratorioArrays/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace LaboratorioArrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    // Arrays tamanho igual
+        int[] arrayUm = new int[100];                         
+        int[] arrayDois = new int[100]; 
+
+    //Popular Array
+        for (int i = 0; i < 100; i++)
+            {
+                arrayUm[i] = i + 1;
+            }
+    //Copiar arrays
+    Array.Copy (arrayUm,arrayDois,100);
+
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine("Indice = " + i + " & Valor = " + arrayDois[i]);
+    }
+
+
+
+
+        int[] array = new int[5] { 10, 20, 30, 40, 50 };
+       // int i = 0;
+        foreach (int valor in array)
+        {
+        Console.WriteLine("Indice = " +  + valor);
+        
+        }
+
+    string[] str = new string[3];
+    int iStr= 0;
+    str[0] = "Um";
+    str[1] = "Dois";
+    str[2] = "Tres";
+    foreach (string valor in str)
+    {
+    Console.WriteLine("Indice = " + iStr + " & Valor = " + valor);
+    iStr++;
+    }
+
+    DateTime[] dt = new DateTime[2];
+    int iDate = 0;
+    dt[0] = new DateTime(2002, 5, 1);
+    dt[1] = new DateTime(2002, 6, 1);
+
+    foreach (DateTime valor in dt)
+    {
+    Console.WriteLine("Indice = " + iDate + " & Valor = " + valor);
+    iDate++;
+    }
+    
+
+    }
+}
